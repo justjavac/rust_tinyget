aa6522d198a2afcb6475cbf79a53d0820fd8588c
迷渡 <justjavac@gmail.com>
2021-01-28 13:17:47 +0800

    fix http
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- examples/http.rs
+++ examples/http.rs
@@ -1,5 +1,5 @@
 fn main() -> Result<(), tinyget::Error> {
-    let response = tinyget::get("https://httpbin.org/anything").send()?;
+    let response = tinyget::get("http://httpbin.org/anything").send()?;
     let hello = response.as_str()?;
     println!("{}", hello);
     Ok(())
