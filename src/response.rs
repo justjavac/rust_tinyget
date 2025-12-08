e25ad3866edece8a94c0229622a0955f510369c5
迷渡 <justjavac@gmail.com>
2025-12-09 06:00:54 +0800

    fix: correct syntax in default reason phrase for status line parsing

--- src/response.rs
+++ src/response.rs
@@ -429,7 +429,7 @@ fn parse_status_line(line: &str) -> (i32, String) {
         if !reason_phrase.is_empty() {
             return (status_code, reason_phrase);
         } else {
-            return (status_code, "Server did not provide a reason".to_string())   
+            return (status_code, "Server did not provide a reason".to_string());
         }
     }
 
